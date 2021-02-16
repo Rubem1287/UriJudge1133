c2891dbcb00104792d54e08e5699191a99165339
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:22:50 -0300

    Add files via upload

---
+++ Uri1133/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Uri1133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, menor, maior, soma;
+
+            soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x >= y)
+            {
+                maior = x;
+                menor = y;
+            }
+            else
+            {
+                maior = y;
+                menor = x;
+            }
+
+            for (int c = menor; c < maior; c++)
+            {
+                if (c % 5 == 2 ||c%5 ==3 )
+                {
+                    Console.WriteLine(c);
+                }
+
+
+            }
+        }
+    }
+}
